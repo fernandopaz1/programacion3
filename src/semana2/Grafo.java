62a00d1a5ac1b97e6e6fdd534b48fe06fa0cd6da
Fernando Paz <ferpaz9292@gmail.com>
2020-09-21 22:10:42 -0300

    agrego metodos agregarArista y existeArista

--- src/semana2/Grafo.java
+++ src/semana2/Grafo.java
@@ -1,13 +1,23 @@
 package semana2;
 
 public class Grafo {
-	
-	private boolean[][] _A; //matriz de adyacencia
 
-	//El  grafo se construye a partir de la cantidad de vertices
+	private boolean[][] _A; // matriz de adyacencia
+
+	// El grafo se construye a partir de la cantidad de vertices
 	public Grafo(int vertices) {
 		_A = new boolean[vertices][vertices];
-		//El grafo se crea sin aristas
+		// El grafo se crea sin aristas
 	}
 
+	// Agregando aristas
+
+	public void agregarArista(int i, int j) {
+		_A[i][j] = true;
+		_A[j][i] = true;
+	}
+
+	public boolean existeArista(int i,int j) {
+		return _A[i][j];
+	}
 }
