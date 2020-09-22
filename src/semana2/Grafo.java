148bc890078c319465cafb041787c8232c1a7a0d
Fernando Paz <ferpaz9292@gmail.com>
2020-09-21 23:42:12 -0300

    agrego test que registran excepciones

--- src/semana2/Grafo.java
+++ src/semana2/Grafo.java
@@ -13,12 +13,43 @@ public class Grafo {
 	// Agregando aristas
 
 	public void agregarArista(int i, int j) {
+		// codigo defensivo, reaccionamos frente a inputs
+		// no deseados
+		verificarVertice(i);
+		verificarVertice(j);
+		verificarDistintos(i, j);
+
 		A[i][j] = true;
 		A[j][i] = true;
 	}
+	
+	public void borrarArista(int i, int j) {
+		// codigo defensivo, reaccionamos frente a inputs
+		// no deseados
+		verificarVertice(i);
+		verificarVertice(j);
+		verificarDistintos(i, j);
 
-	public boolean existeArista(int i,int j) {
+		A[i][j] = false;
+		A[j][i] = false;
+	}
+
+	private void verificarDistintos(int i, int j) {
+		if(i==j)
+			throw new IllegalArgumentException("No se permiten loops: (" +"i="+i +", j="+j);
+	}
+
+	private void verificarVertice(int i) {
+		if (i < 0)
+			throw new IllegalArgumentException("El vertice no puede ser negativo: " + i);
+		if (i >= A.length)
+			throw new IllegalArgumentException("El vertice no existe: " + i);
+	}
+
+	public boolean existeArista(int i, int j) {
+		verificarVertice(i);
+		verificarVertice(j);
 		return A[i][j];
 	}
-	  
+
 }
--- src/semana2/GrafoTest.java
+++ src/semana2/GrafoTest.java
@@ -6,9 +6,56 @@ import org.junit.Test;
 
 public class GrafoTest {
 
+	@Test(expected = IllegalArgumentException.class)
+	public void primerVerticeNegativoTest() {
+		Grafo grafo = new Grafo(5);
+		grafo.agregarArista(-1, 3);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void primerVerticeExcedidoTest() {
+		Grafo grafo = new Grafo(5);
+		grafo.agregarArista(5, 3);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void segundoVerticeNegativoTest() {
+		Grafo grafo = new Grafo(5);
+		grafo.agregarArista(1, -3);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void segundoVerticeExcedidoTest() {
+		Grafo grafo = new Grafo(5);
+		grafo.agregarArista(1, 5);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void agregarLoopsTest() {
+		Grafo grafo = new Grafo(5);
+		grafo.agregarArista(1, 1);
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testExisteArista() {
+		Grafo grafo = new Grafo(5);
+		grafo.agregarArista(2, 3);
+		assertTrue(grafo.existeArista(2, 3));
+		assertFalse(grafo.existeArista(1, 1));
+	}
+
+	@Test
+	public void testExisteAristaOpuesta() {
+		Grafo grafo = new Grafo(5);
+		grafo.agregarArista(2, 3);
+		assertTrue(grafo.existeArista(3, 2));
+	}
+
+	@Test
+	public void testAristaInexistente() {
+		Grafo grafo = new Grafo(5);
+		grafo.agregarArista(2, 3);
+		assertFalse(grafo.existeArista(1, 1));
 	}
 
 }
