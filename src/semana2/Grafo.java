425ebbc88505480460645231891d0d5411e0e49e
Fernando Paz <ferpaz9292@gmail.com>
2020-09-21 22:39:11 -0300

    agrego test GrafoTest con JUnit 4

--- src/semana2/Grafo.java
+++ src/semana2/Grafo.java
@@ -2,22 +2,23 @@ package semana2;
 
 public class Grafo {
 
-	private boolean[][] _A; // matriz de adyacencia
+	private boolean[][] A; // matriz de adyacencia
 
 	// El grafo se construye a partir de la cantidad de vertices
 	public Grafo(int vertices) {
-		_A = new boolean[vertices][vertices];
+		A = new boolean[vertices][vertices];
 		// El grafo se crea sin aristas
 	}
 
 	// Agregando aristas
 
 	public void agregarArista(int i, int j) {
-		_A[i][j] = true;
-		_A[j][i] = true;
+		A[i][j] = true;
+		A[j][i] = true;
 	}
 
 	public boolean existeArista(int i,int j) {
-		return _A[i][j];
+		return A[i][j];
 	}
+	  
 }
